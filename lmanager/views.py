569142ca356c261fd71fe1555b7aeb969f0b9f66
Raihan Ahmed <raihan.tusher@yahoo.com>
2022-07-07 14:13:04 +0600

    blank views

--- lmanager/views.py
+++ lmanager/views.py
@@ -7,3 +7,63 @@ def home(request):
     return render(request, 'backend/dashboard.html')
 
 
+# cat operation starts here
+
+def list_category(request):
+    return render(request, 'backend/category/list.html')
+
+
+def add_category(request):
+    return render(request, 'backend/category/add.html')
+
+
+def edit_category(request):
+    return render(request, 'backend/category/add.html')
+
+
+def del_category(request, id):
+    return render(request, 'uc.html')
+
+
+# cat operation ends here
+
+
+# cat operation starts here
+
+def list_book(request):
+    return render(request, 'backend/book/list.html')
+
+
+def add_book(request):
+    return render(request, 'backend/book/add.html')
+
+
+def edit_book(request, id):
+    return render(request, 'backend/book/add.html')
+
+
+def del_book(request, id):
+    return render(request, 'uc.html')
+
+
+# cat operation ends here
+
+
+# cat operation starts here
+
+def list_dept(request):
+    return render(request, 'backend/dept/list.html')
+
+
+def add_dept(request):
+    return render(request, 'backend/dept/add.html')
+
+
+def edit_dept(request):
+    return render(request, 'backend/dept/add.html')
+
+
+def del_dept(request, id):
+    return render(request, 'uc.html')
+
+# cat operation ends here
