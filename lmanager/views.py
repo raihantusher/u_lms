32b2a052f63fc29519cd772340d6bee5543194e2
Raihan Ahmed <raihan.tusher@yahoo.com>
2022-07-07 15:03:07 +0600

    category add view done

--- lmanager/views.py
+++ lmanager/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
+from student.models import Department, Student
+from book.models import Book, Category
+
+from student.forms import DeptForm
+from book.forms import BookForm, CatForm
 
 # Create your views here.
 
@@ -14,7 +20,20 @@ def list_category(request):
 
 
 def add_category(request):
-    return render(request, 'backend/category/add.html')
+    cat_form = CatForm()
+
+    if request.method == "POST":
+        cat_form = CatForm(request.POST, request.FILES)
+        if cat_form.is_valid():
+            cat_form.save()
+            messages.success(request, ' Category is created successfully!')
+            redirect('list_cat')
+
+    context = {
+        'category_form': cat_form
+    }
+
+    return render(request, 'backend/category/add.html', context)
 
 
 def edit_category(request):
