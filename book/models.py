8ace7d5e3bc8ec1f73df06e5d4a436d4f59ef40d
Raihan Ahmed <raihan.tusher@yahoo.com>
2022-07-08 10:27:23 +0600

    category model name

--- book/models.py
+++ book/models.py
@@ -7,6 +7,9 @@ class Category(models.Model):
     name = models.CharField(max_length=150)
     is_active = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Book(models.Model):
     image = models.ImageField()
