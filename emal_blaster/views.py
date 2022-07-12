fae139aebac24bb1ef8a638d66699ff5a0a4a280
Raihan Ahmed <raihan.tusher@yahoo.com>
2022-07-12 09:29:52 +0600

    single book

--- emal_blaster/views.py
+++ emal_blaster/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import  render
+from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
+from book.models import Book
 
 def Home(request):
     return render(request, 'frontend/home.html')
+
+
+def Details(request, id):
+    detail = Book.objects.get(pk=id)
+    context = {
+        'single_book': detail
+    }
+    return render(request, 'frontend/details.html', context)
 
 
 def advertisment(request):
