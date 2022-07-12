8d5f649fc4e17404bffe86759814655acd2faf66
Raihan Ahmed <raihan.tusher@yahoo.com>
2022-07-12 10:10:23 +0600

    media URL

--- emal_blaster/urls.py
+++ emal_blaster/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 urlpatterns = [
@@ -27,4 +29,4 @@ urlpatterns = [
     path('portal/', include('student.urls')),
 
     path('acc/', include('accounts.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
