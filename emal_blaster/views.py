112e94c966970808b541359e3fb1fcf7b2215a31
Raihan Ahmed <raihan.tusher@yahoo.com>
2022-07-13 14:09:22 +0600

    book request view

--- emal_blaster/views.py
+++ emal_blaster/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
 from book.models import Book
+from student.models import Book_Student
+
 
 def Home(request):
     return render(request, 'frontend/home.html')
-
 
 def Details(request, id):
     detail = Book.objects.get(pk=id)
@@ -13,6 +14,18 @@ def Details(request, id):
         'single_book': detail
     }
     return render(request, 'frontend/details.html', context)
+
+
+def book_request(request, id):
+    if request.method == "POST" and request.user.is_authenticated == True:
+        book = Book.objects.filter(pk=id, available=True).last()
+        # print(request.POST.get('book_id'))
+        context = {
+            'book': book
+        }
+
+        return render(request, 'frontend/request_success.html', context)
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
 def advertisment(request):
