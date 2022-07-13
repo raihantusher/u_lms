8869e2990eeb346208fc501d1cb5a1848a10e7b3
Raihan Ahmed <raihan.tusher@yahoo.com>
2022-07-13 17:49:46 +0600

    home views

--- student/views.py
+++ student/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from .models import BookStudent
-
+from book.models import Book
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'student/portal.html')
+    not_returned = BookStudent.objects.filter(student=request.user.student,  is_returned=False).count()
+    total_books = Book.objects.all().count()
+
+    context = {
+        'not_returned':not_returned,
+        'total_books':total_books
+    }
+    return render(request, 'student/portal.html', context)
 
 
 def issued_book(request):
