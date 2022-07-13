e88a89b965c15f99c38be03c38fb15d4b738518a
Raihan Ahmed <raihan.tusher@yahoo.com>
2022-07-13 15:07:03 +0600

    book request views

--- emal_blaster/views.py
+++ emal_blaster/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.core.exceptions import ObjectDoesNotExist
 
 from book.models import Book
-from student.models import Book_Student
+from student.models import BookStudent
 
 
 def Home(request):
     return render(request, 'frontend/home.html')
+
 
 def Details(request, id):
     detail = Book.objects.get(pk=id)
@@ -18,7 +20,22 @@ def Details(request, id):
 
 def book_request(request, id):
     if request.method == "POST" and request.user.is_authenticated == True:
-        book = Book.objects.filter(pk=id, available=True).last()
+        book = Book.objects.filter(pk=id).last()
+        student = request.user.student
+
+        book_student = None
+        if book.available:
+            book_student = BookStudent.objects.filter(book=book, student=student).last()
+
+            if book_student.is_returned == True:
+                print("book student is returned ")
+                BookStudent.objects.create(book=book, student=student)
+            elif book_student.is_returned == False:
+                pass
+            else:
+                print(" Book is not available")
+                BookStudent.objects.create(book=book, student=student)
+
         # print(request.POST.get('book_id'))
         context = {
             'book': book
